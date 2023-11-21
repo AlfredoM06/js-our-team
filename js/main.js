fb54b8f867abe520d732143141465ae9df250197
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-21 17:20:18 +0100

    stage 2

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@
 
 let teamMembers = [
      {
-        photo: "",
+        photo: "angela-caroll-chief-editor.jpg",
         name: "Angela Caroll",
         job: " Chief Editor"
     },
     {
-        photo: "",
+        photo: "angela-lopez-social-media-manager.jpg",
         name: "Angela Lopez",
         job: " Socia Media Manager"
     },
@@ -35,3 +35,24 @@ let teamMembers = [
         job: "Founder & CEO"
     },
 ];
+
+
+document.getElementById("team_cards").innerHTML += `
+    <div class="card col-4">
+        <img src="./img/${teamMembers[0].photo}" class="card-img-top" alt="Angela">
+        <div class="card-body">
+            <p class="card-text">${teamMembers[0].name}</p>
+            <p>${teamMembers[0].job}</p>
+        </div>
+    </div>
+
+`
+document.getElementById("team_cards").innerHTML += `
+    <div class="card col-4">
+        <img src="./img/${teamMembers[1].photo}" class="card-img-top" alt="Angela">
+        <div class="card-body">
+            <p class="card-text">${teamMembers[1].name}</p>
+            <p>${teamMembers[1].job}</p>
+        </div>
+    </div>
+`
