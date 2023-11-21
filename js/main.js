b642895ad9c6d9fa4c9e5ba97ec89d119c90e08e
AlfredoM06 <Alfredomanfredi06@gmail.com>
2023-11-21 15:42:41 +0100

    added foto and started with milestone 0

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// esercizio: crea un array di oggetti per rappresentare i membri del team utilizzando idati forniti.
+// ogni membro è caratterizzato dalle seguenti info: nome, ruolo e foto.
+
+//milestone 0: creare l'array di oggetti con le info fornite nella tabella qui sotto.
+
+let teamMembers = [
+     {
+        foto: "",
+        name: "Angela Caroll",
+        job: " Chief editor"
+    },
+    {
+        foto: "",
+        name: "Angela Lopez",
+        job: " Socia media manager"
+    },
+    {
+        foto: "",
+        name: "Barbara Ramos",
+        job: "Graphic designer"
+    },
+    {
+        foto: "",
+        name: "Scott Estrada",
+        job: "Developer"
+    },
+    {
+        foto: "",
+        name: "Walter Gordon",
+        job: "Office manager"
+    },
+    {
+        foto: "",
+        name: "Wayne Barnett",
+        job: "Founder"
+    },
+];
